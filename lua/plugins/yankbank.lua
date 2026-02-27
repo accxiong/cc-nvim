51ff9b8cceb46367c105a09b733c79785b5c6c70
小阿熊 <786507017@qq.com>
2026-02-27 11:03:37 +0800

    feat: 添加 yankbank

---
+++ lua/plugins/yankbank.lua
@@ -0,0 +1,26 @@
+return {
+  'ptdewey/yankbank-nvim',
+  dependencies = 'kkharji/sqlite.lua',
+  cmd = { 'YankBank' },
+  keys = {
+    { '<leader>y', '<CMD>YankBank<CR>', desc = 'Open YankBank' },
+    { 'y',         mode = { 'n', 'x' } },
+    { 'Y',         mode = { 'n', 'x' } },
+  },
+  config = function()
+    require('yankbank').setup({
+      max_entries = 10,
+      sep = '---------------------------------------------------------------------------------------------------------',
+      num_behavior = 'jump',
+      focus_gain_poll = true,
+      persist_type = 'sqlite',
+      keymaps = {
+        paste = '<CR>',
+        paste_back = 'P',
+      },
+      registers = {
+        yank_register = '+',
+      },
+    })
+  end,
+}
